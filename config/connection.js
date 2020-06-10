9ab63b998c67185dd2b77b6a4d13594710f7e018
Drew Congino <dcongino@yahoo.com>
2020-06-10 11:10:20 -0600

    Jaws DB fixed

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 // Make connection.
 connection.connect(function (err) {
     if (err) {
@@ -15,16 +19,5 @@ connection.connect(function (err) {
     }
     console.log("connected as id " + connection.threadId);
 });
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "hacktheplanet",
-        database: "todoagain_db"
-    });
-};
 // Export connection for our ORM to use.
 module.exports = connection;
