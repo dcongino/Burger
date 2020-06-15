1915c947d69badfdb29c271a929c9c3243e6e938
Drew Congino <dcongino@yahoo.com>
2020-06-15 16:53:12 -0600

    Added Order Button

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -20,7 +20,30 @@ $(function() {
         }
       );
     });
-  
+
+    $(function() {
+      $(".addburger").on("click", function(event) {
+        var id = $(this).data("id");
+        
+    
+        var newDevouredState = {
+          devoured: 0
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevouredState
+        }).then(
+          function() {
+            console.log("burger ordered");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+
+    
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
@@ -43,4 +66,4 @@ $(function() {
       );
     });
   });
-  
+});
